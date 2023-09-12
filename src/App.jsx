7435ce671feceba827d988bdf72a8b113dd38436
MD MASUM <mdmasumdu45@gmail.com>
2023-09-12 19:48:12 +0600

    aded

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='font-bold text-5xl text-center mt-10'>Knowledge Cafe</h1>
+     
+       
+    </>
+  )
+}
+
+export default App
